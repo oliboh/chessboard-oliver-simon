6350aa5ffe034b8b07c3698876abd1c90fef8b2a
Oliver Bohman <oliver.bohman95@gmail.com>
2026-09-09 08:43:10 +0200

    Validate chessboard size input

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hej och välkommen till ChessBoard.");
+Console.Write("Hur stort schackbräde vill du skapa? Ange ett heltal mellan 3-50: ");
+Int32.TryParse(Console.ReadLine(), out int number);
+if (number >= 3 && number <= 50)
+{
+    Console.WriteLine($"Du valde {number}");
+}
+else
+{
+    Console.WriteLine("Felaktig inmatning, försök igen. ");
+}
