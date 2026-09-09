e09854fd876bc77dc65b1d45842786a0079e9a25
Seldion <simon.hansson@seldion.dev>
2026-09-09 09:41:08 +0200

    Render basic chessboard pattern

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,9 +1,28 @@
-﻿Console.WriteLine("Hej och välkommen till ChessBoard.");
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("Hej och välkommen till ChessBoard.");
 Console.Write("Hur stort schackbräde vill du skapa? Ange ett heltal mellan 3-50: ");
 Int32.TryParse(Console.ReadLine(), out int number);
 if (number >= 3 && number <= 50)
 {
     Console.WriteLine($"Du valde {number}");
+
+    for (int rows = 0; rows < number;  rows++)
+    {
+        for (int columns = 0; columns < number; columns++)
+        {
+            if ((rows + columns) % 2 == 0)
+            {
+                Console.Write("X");
+            } 
+            else
+            {
+                Console.Write("O");
+            } 
+        }
+        Console.Write("\n");
+    }
+
 }
 else
 {
