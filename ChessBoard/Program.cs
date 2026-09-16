42cfe8c427f83b9657dd3fea5d49f168799c7975
Oliver Bohman <oliver.bohman95@gmail.com>
2026-09-16 09:19:30 +0200

    Added main method

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -1,17 +1,24 @@
-﻿using System.Runtime.InteropServices;
-Console.OutputEncoding = System.Text.Encoding.Unicode;
-Console.WriteLine("Hej och välkommen till ChessBoard.");
-Console.Write("Hur stort schackbräde vill du skapa? Ange ett heltal mellan 3-50: ");
-Int32.TryParse(Console.ReadLine(), out int number);
-if (number >= 3 && number <= 50)
-{
-    Console.WriteLine($"Du valde {number}");
+﻿namespace ChessBoard;
 
-    ChessBoard.ChessBoard chessboard = new ChessBoard.ChessBoard(number);
-    ChessBoard.ChessBoard.RenderBoard(chessboard);
-
-}
-else
+public class Program
 {
-    Console.WriteLine($"Felaktig inmatning, numret: {number} är ogiltigt, numret måste vara mellan 3 - 50. försök igen. ");
+    public static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.WriteLine("Hej och välkommen till ChessBoard.");
+        Console.Write("Hur stort schackbräde vill du skapa? Ange ett heltal mellan 3-50: ");
+        Int32.TryParse(Console.ReadLine(), out int number);
+        if (number >= 3 && number <= 50)
+        {
+            Console.WriteLine($"Du valde {number}");
+
+            ChessBoard chessboard = new ChessBoard(number);
+            ChessBoard.RenderBoard(chessboard);
+
+        }
+        else
+        {
+            Console.WriteLine($"Felaktig inmatning, numret: {number} är ogiltigt, numret måste vara mellan 3 - 50. försök igen. ");
+        }
+    }
 }
