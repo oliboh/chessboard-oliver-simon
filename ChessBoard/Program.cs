e8972c8ac6eef2aa42c8959b17145b5324045a39
Oliver Bohman <oliver.bohman95@gmail.com>
2026-09-16 09:27:44 +0200

    Added class StandardMessages

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -5,8 +5,11 @@ public class Program
     public static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
-        Console.WriteLine("Hej och välkommen till ChessBoard.");
-        Console.Write("Hur stort schackbräde vill du skapa? Ange ett heltal mellan 3-50: ");
+              
+        StandardMessages.GreetUser();
+
+        StandardMessages.AskForInput();
+
         Int32.TryParse(Console.ReadLine(), out int number);
         if (number >= 3 && number <= 50)
         {
---
+++ ChessBoard/StandardMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessBoard
+{
+    public class StandardMessages
+    {
+        public static void GreetUser()
+        {
+            Console.WriteLine("Hej och välkommen till ChessBoard.");
+        }
+        public static void AskForInput()
+        {
+            Console.Write("Hur stort schackbräde vill du skapa? Ange ett heltal mellan 3 - 50: ");
+        }
+    }
+}
